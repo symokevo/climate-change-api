8a9ec5cbf08a866f624678150a976b986c080df1
Simon Rabuogi <46306463+symokevo@users.noreply.github.com>
2021-10-21 13:39:07 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+// const PORT = 8000; // creat the server listening port
+const PORT = process.env.port || 8000 // for heroku deployment
+const express = require('express'); // add express to the project
+const axios = require('axios'); // add axios to the project
+const cheerio = require('cheerio'); // add cheerio to the project
+
+const app = express(); // utilize the express
+
+const articles = []; // store the fetched aritles in array
+
+const newspapers = [
+    { 
+        name:'thetimes',
+        address: 'http://thetimes.co.uk/environment/climate-change',
+        base: ''
+    },
+    { 
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+        base: ''
+    },
+    { 
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+        base: 'https://www.telegraph.co.uk'
+    }
+];
+app.listen(PORT, () => console.log('server is listening on port ' +PORT));
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        $('a:contains("climate")', html).each(function() {
+            const title = $(this).text();
+            const url = $(this).attr('href');
+
+            articles.push({
+                title,
+                url: newspaper.base + url,
+                source: newspaper.name
+            })
+        })
+    })
+})
+
+app.get('/', (req, res) => {
+    res.json('Welcome to my climate change news API')
+});
+
+app.get('/news', (req, res) => {
+    res.json(articles);
+})
+
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId;
+    const newspaperAdress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address;
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base;
+
+    axios.get(newspaperAdress)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const specificArticles = [];
+
+        $('a:contains("climate")', html).each(function() {
+            const title = $(this).text();
+            const url = $(this).attr('href');
+
+            specificArticles.push({
+                title, 
+                url: newspaperBase + url,
+                source: newspaperId
+            })
+        })
+        res.json(specificArticles)
+    }).catch(err => console.log(err))
+})
